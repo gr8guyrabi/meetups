1729fdc638620437da79c39580878d97210a9311
Rabi Maharjan <gr8guyrabi@gmail.com>
2021-09-04 09:06:37 +0545

    add: meetups homepage

--- meetups/views.py
+++ meetups/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -7,4 +6,19 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Hello World!')
+    meetups = [
+        {
+            'title': 'A First Meetup',
+            'location': 'Kathmandu',
+            'slug': 'a-first-meetup'
+        },
+        {
+            'title': 'A Second Meetup',
+            'location': 'Pokhara',
+            'slug': 'a-second-meetup'
+        }
+    ]
+    return render(request, 'meetups/index.html', {
+        'showMeetup': True,
+        'meetups': meetups
+    })

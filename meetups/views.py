87421b028002e8d31659fe4242512e5e0032081b
Rabi Maharjan <gr8guyrabi@gmail.com>
2021-09-05 23:43:07 +0545

    feat: add participant form in detail

--- meetups/views.py
+++ meetups/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from .models import Meetup
 
+from .forms import RegistrationForm
+
 # Create your views here.
 
 # Every view is a function to be called.
@@ -17,9 +19,11 @@ def index(request):
 def detail(request, meetup_slug):
     try:
         meetup_detail = Meetup.objects.get(slug=meetup_slug)
+        registration_form = RegistrationForm()
         return render(request, 'meetups/detail.html', {
             'meetup_available': True,
             'meetup': meetup_detail,
+            'registration_form': registration_form,
         })
     except Exception as e:
         return render(request, 'meetups/detail.html', {

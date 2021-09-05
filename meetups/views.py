276dbd63e0357336c24ec44ce8830f29da4db462
Rabi Maharjan <gr8guyrabi@gmail.com>
2021-09-05 22:57:36 +0545

    fix: display location and image to the view

--- meetups/views.py
+++ meetups/views.py
@@ -17,11 +17,13 @@ def index(request):
 def detail(request, meetup_slug):
     try:
         meetupDetail = Meetup.objects.get(slug=meetup_slug)
-
+        print(meetupDetail)
         return render(request, 'meetups/detail.html', {
             'meetup_available': True,
             'meetup_title': meetupDetail.title,
-            'meetup_description': meetupDetail.description
+            'meetup_description': meetupDetail.description,
+            'meetup_image': meetupDetail.image.url,
+            'meetup_location': meetupDetail.location,
         })
     except Exception as e:
         return render(request, 'meetups/detail.html', {

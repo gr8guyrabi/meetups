3ae6ac5550abd4e6fe3974740c7a37245ec135b4
Rabi Maharjan <gr8guyrabi@gmail.com>
2021-09-07 13:07:57 +0545

    refact: use generic RedirectView for homepage /

--- meetups/urls.py
+++ meetups/urls.py
@@ -4,8 +4,8 @@ from . import views
 # map url to app view
 urlpatterns = [  # urlpatterns <- has to be a list name
     # map domain_name.com/meetups/ to response from index function of views.py
-    path('meetups/', views.index, name='all-meetups'),
-    path('meetups/<slug:meetup_slug>/registration-success',
+    path('', views.index, name='all-meetups'),
+    path('<slug:meetup_slug>/registration-success',
          views.registration_success, name='registration-success'),
-    path('meetups/<slug:meetup_slug>', views.detail, name='meetup-detail'),
+    path('<slug:meetup_slug>', views.detail, name='meetup-detail'),
 ]
